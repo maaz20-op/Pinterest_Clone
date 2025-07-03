f4f9c987f4345f86ec1a546b74adfd3dd444b0a1
Maaz Javed <maazcoding54@gmail.com>
2025-07-04 01:05:05 +0500

    new

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
 
-const generateToken = (email)=>{
-  return jwt.sign({email},process.env.JWT_SECRET)
-}
+const generateToken = (email) => {
+  return jwt.sign({ email }, process.env.JWT_SECRET, {
+    expiresIn: "1d", 
+  });
+};
 
 module.exports = generateToken;
-
 
 
 
