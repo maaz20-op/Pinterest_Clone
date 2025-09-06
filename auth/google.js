e982b97fb8e136f5c180978e790aba1648f39352
unknown <maazcoding54@gmail.com>
2025-09-06 20:45:20 +0500

    addedfle

--- auth/google.js
+++ auth/google.js
@@ -9,7 +9,7 @@ const { googleConfigCallback } =  require("../controllers/authController");
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret:process.env.GOOGLE_CLIENT_ID,
+  clientSecret:process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: 'http://localhost:3000/auth/google/callback',
 }, googleConfigCallback));
 
