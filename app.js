d5ec1ce1cb1bbe1c5ccb502f0577a0837701bf25
unknown <maazcoding54@gmail.com>
2025-09-06 21:21:11 +0500

    hhrhgergrey

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-  
+  console.log(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.log("❌ MongoDB Error:", err));
--- auth/google.js
+++ auth/google.js
@@ -5,8 +5,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const { googleCallback }  =  require("../controllers/authController");
 const { googleConfigCallback } =  require("../controllers/authController");
 
-
-
+console.log(process.env.GOOGLE_CLIENT_ID)
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret:process.env.GOOGLE_CLIENT_SECRET,
